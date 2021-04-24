eb3a7a73c2a14626895c7f6c5a4a7467ea311f1e
ravikiranmaroju <36310929+ravikiranmaroju@users.noreply.github.com>
2021-04-25 02:54:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
-* This is a simple Java program.
-   FileName : "HelloWorld.java". */
-class HelloWorld
-{
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
-    }
-}
+public class Hello {
+     public static void main(string[] args) {
+           // Prints “Hello, World” in the terminal window.
+           System.out.printIn(“Hello, World”);
+         }
+ }
