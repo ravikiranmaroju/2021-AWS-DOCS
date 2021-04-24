d7ed7e44adbe44cd96daa8bb22379203fa34d28f
ravikiranmaroju <36310929+ravikiranmaroju@users.noreply.github.com>
2021-04-25 02:48:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+* This is a simple Java program.
+   FileName : "HelloWorld.java". */
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
