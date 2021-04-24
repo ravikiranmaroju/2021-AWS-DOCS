f4143502c181aea3f8918c2ddfe8a2fb368105a1
ravikiranmaroju <36310929+ravikiranmaroju@users.noreply.github.com>
2021-04-25 02:59:04 +0530

    Update and rename Hello.java to HelloWorld.java

--- Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-     public static void main(string[] args) {
-           // Prints “Hello, World” in the terminal window.
-           System.out.printIn(“Hello, World”);
-         }
- }
